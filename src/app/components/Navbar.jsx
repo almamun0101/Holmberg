2343fbe8503bb212e8b2205fa51577b08dd2628b
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-08-02 02:12:15 +0600

    next js nav

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,25 @@
+"use client"
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+            <div className="flex justify-between px-5 py-2 items-center ">
+                <img src="./Images/logo.png" alt="" />
+                <div className="">
+                    <ul className='flex gap-5 '>
+                        <li>Home</li>
+                        <li>About </li>
+                        <li>Contact</li>
+                        <li>Servie</li>
+                    </ul>
+                </div>
+                <button className='bg-primary rounded-2xl text-white px-4 text-sm font-medium py-1'>
+                    Sign Up 
+                </button>
+            </div>
+    </div>
+  )
+}
+
+export default Navbar
